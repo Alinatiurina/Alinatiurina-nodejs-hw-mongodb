531d8042fa73ce92462d57eaf3294588f9289cdb
Alinatiurina <tiyrina.alina@gmail.com>
2024-06-18 12:38:36 +0300

    hw2

---
+++ src/db/models/Contact.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    isFavourite: Boolean,
+    contactType: {
+        type: String,
+        enum: ["work", "home", "personal"],
+        default: 'personal',
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+const Contact = model("contact", contactSchema);
+
+export default Contact;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
+
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
 import env from "./utils/env.js";
-
-import contacts from "./contacts.json";
+import { getContact, getContactById } from "./services/contact-service.js";
 
 const port = env("PORT", "3000");
 
@@ -19,17 +19,48 @@ const setupServer = ()=> {
     app.use(logger);
     app.use(cors());
     
-    app.get("/api/contacts", (req, res)=> {
-        res.json(contacts);
-    })
+    app.get('/api/contacts', async(req, res) => {
+        const data = await getContact();
+        res.json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data,
+        });
+    });
+    
+    app.get("/api/contacts/:id", async (req, res) => {
+        try {
+            const { id } = req.params;
+            const data = await getContactById(id);
 
-    app.use((req, res)=> {
+            if (!data) {
+                return res.status(404).json({
+                    message: `Contact with id=${id} not found`
+                });
+            }
+            res.json({
+                status: 200,
+                message: `Successfully found contact id=${id}!`,
+                data,
+            });  
+        } catch (error) {
+            if (error.message.includes("Cast to ObjectID failed")) {
+                error.status = 404;
+            }
+            const { status = 500 } = error;
+            res.status(status).json({
+                message: error.message
+            });
+        }
+    });
+
+    app.use((req, res) => {
         res.status(404).json({
             message: "Not Found"
-        })
-    })
+        });
+    });
 
-    app.listen(port, ()=> console.log(`Server running on ${port} PORT`))
-}
+    app.listen(port, () => console.log(`Server running on ${port} PORT`));
+};
 
 export default setupServer;
